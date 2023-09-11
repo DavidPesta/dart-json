00283f170fec45c842f4387ef39265cb85021298
David Pesta <davidpesta@gmail.com>
2023-09-10 21:58:51 -0500

    work in progress

--- SmellySock.dart
+++ SmellySock.dart
@@ -8,6 +8,19 @@ class SmellySock extends Model {
 	
 	SmellySock({required this.color, required this.smelliness});
 	
+	factory SmellySock.fromJson(String json) {
+		var decodedMap = jsonDecode(json);
+		return SmellySock.fromMap(decodedMap);
+	}
+	
+	factory SmellySock.fromMap(Map<String, dynamic> map) {
+		print(map);
+		return new SmellySock(
+			color: Color.blue,
+			smelliness: 1,
+		);
+	}
+	
 	Map<String, dynamic> toMap() {
 		return {
 			'color': color,
--- run.dart
+++ run.dart
@@ -5,12 +5,86 @@ import 'Cube.dart';
 import 'SmellySock.dart';
 
 void main() {
-
+	
 	// Phase 1: I got toJson working to my satisfaction!
 	
-	var s = new SmellySock(color: Color.green, smelliness: 7);
-	var c = new Cube(level: 1, moods: {true: Mood.happy}, fun: {52.3: "yay"}, smellySocks: {3: s});
-	var hc = new HyperCube(name: "Happy", cubes: {Color.red: [[{"good": [{Phase.matter: c}]}]]});
+	//var s = new SmellySock(color: Color.green, smelliness: 7);
+	//var c = new Cube(level: 1, moods: {true: Mood.happy}, fun: {52.3: "yay"}, smellySocks: {3: s});
+	// var hc = new HyperCube(name: "Happy", cubes: {Color.red: [[{"good": [{Phase.matter: c}]}]]});
 	
-	print(hc.toJson());
+	//print(hc.toJson());
+	
+	
+	
+	// Phase 2: Figuring out how to do fromJson
+	
+	var s_json = """
+		{
+			"color": "Color.green",
+			"smelliness": 7
+		}
+	""";
+	
+	var s = SmellySock.fromJson(s_json);
+	print(s.toJson());
+	
+	
+	
+	var c_json = """
+		{
+			"level": 1,
+			"moods": {
+				"true": "Mood.happy"
+			},
+			"fun": {
+				"52.3": "yay"
+			},
+			"smellySocks": {
+				"3": {
+					"color": "Color.green",
+					"smelliness": 7
+				}
+			}
+		}
+	""";
+	
+	var hc_json = """
+		{
+			"name": "Happy",
+			"cubes": {
+				"Color.red": [
+					[
+						{
+							"good": [
+								{
+									"Phase.matter": {
+										"level": 1,
+										"moods": {
+											"true": "Mood.happy"
+										},
+										"fun": {
+											"52.3": "yay"
+										},
+										"smellySocks": {
+											"3": {
+												"color": "Color.green",
+												"smelliness": 7
+											}
+										}
+									}
+								}
+							]
+						}
+					]
+				]
+			}
+		}
+	""";
+	
+	
+	
+	//var hc = Model<HyperCube>.fromJson(json);
+	//print(hc.toJson());
+	
+	//Model.fromJson(json);
 }
